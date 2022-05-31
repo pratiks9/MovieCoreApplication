871087cb3b6e527325e32d982c0c62e2f9cf6b88
pratiks9 <shindeps98@gmail.com>
2022-05-31 14:49:38 +0530

    Add project files.

---
+++ MovieApp.Entity/MovieShowTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MovieApp.Entity
+{
+    public class MovieShowTime
+    {
+        //create table MovieShowTime(ShowId int,MovieId int references MovieModel(MovieId))
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ShowId { get; set; }
+        [ForeignKey("movieModel")]
+        public int MovieId { get; set; }
+        public MovieModel movieModel { get; set; }
+        [ForeignKey("theatreModel")]
+        public int TheatreId { get; set; }
+        public TheatreModel theatreModel { get; set; }
+        public string ShowTime { get; set; }
+        public string Date { get; set; }
+    }
+}
+
